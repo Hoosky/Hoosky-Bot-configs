eaae74a099fd260c785b5d29b46b96392e5b7d45
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-05 08:05:23 -0400

    Add files via upload

---
+++ commands/fetch.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("(**runs**)")
+message.channel.send("(**grabs stick**)")
+message.channel.send("(**runs back**)")
+message.channel.send("(**sits**)")
+
+}
+
+module.exports.help = {
+  name: "Fetch"
+}
