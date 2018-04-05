1f4f59284088c9b003832dbe8a1edbb414e97cdf
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-05 07:17:15 -0400

    Add files via upload

---
+++ commands/Hestia.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("https://i.pinimg.com/originals/71/ca/76/71ca760e25f6b45c7e4f9ba843275a22.gif");
+message.channel.send("Hestia is Bestia")
+
+}
+
+module.exports.help = {
+  name: "Hestia"
+}
