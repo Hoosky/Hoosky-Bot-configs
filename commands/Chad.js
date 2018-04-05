4bafab7a60c2796deedfacbf908bf769badafb4f
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-05 07:41:33 -0400

    Add files via upload

---
+++ commands/Chad.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("https://i.imgur.com/z8fZiIA.jpg")
+
+}
+
+module.exports.help = {
+  name: "ChadHoosky"
+}
---
+++ commands/Ruby.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("http://25.media.tumblr.com/0df06df6ba68375584caf7e6b74c5003/tumblr_mvvzprpPDa1t0psroo2_500.png")
+
+}
+
+module.exports.help = {
+  name: "ruby"
+}
