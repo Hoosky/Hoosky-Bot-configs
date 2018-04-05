1d88cce5dc4f4f8334fa2903af1403aa7ff688ce
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-05 07:40:30 -0400

    Add files via upload

---
+++ commands/oof.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("https://www.youtube.com/watch?v=f49ELvryhao")
+
+}
+
+module.exports.help = {
+  name: "oof"
+}
