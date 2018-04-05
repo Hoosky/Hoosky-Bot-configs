db721ff767bc030a5be9f86ec62aa32a1181315c
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-05 07:22:54 -0400

    Add files via upload

---
+++ commands/nursecall.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+if(!message.member.hasPermission("MANAGE_WEBHOOKS")) return message.reply("You're not my Mama! :cold_sweat:");
+message.channel.send("Yes Mama? :3 (**wags tail**) :heart:")
+
+}
+
+module.exports.help = {
+  name: "Hoosky Bot!!"
+}
