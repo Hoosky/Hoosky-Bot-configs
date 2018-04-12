4fbbec4ac97fee4f782470b31caf0e33f7220a10
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-12 13:51:56 -0400

    Update currenttopic.js

--- commands/currenttopic.js
+++ commands/currenttopic.js
@@ -5,7 +5,7 @@ let topic = JSON.parse(fs.readFileSync("./settopic.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!messgae.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, You can't do that.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, You can't do that.");
   let cUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if (!cUser) return message.reply("Could not find set topic.");
   let topic = topic [cUser.id].topic;
