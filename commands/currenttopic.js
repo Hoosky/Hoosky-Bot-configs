474d5b84cb7a1322a3524c73c1ec7c26d08f5c7d
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-04-12 13:47:56 -0400

    Update currenttopic.js

--- commands/currenttopic.js
+++ commands/currenttopic.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let ctopic = JSON.parse(fs.readFileSync("./settopic.json", "utf8"));
+let topic = JSON.parse(fs.readFileSync("./settopic.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
   if(!messgae.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry, You can't do that.");
   let cUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if (!cUser) return message.reply("Could not find set topic.");
-  let ctopic = topic [wUser.id].warns;
+  let topic = topic [cUser.id].topic;
 
-  message.reply(`The current topic is: ${ctopic}`);
+  message.reply(`The current topic is: ${topic}`);
 
 
 }
 
 module.exports.help = {
-  name: "currenttopic"
+  name: "ctopic"
 }
