63233e94f654ddf0c7a2aa02eac44238380d96e6
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-06-17 12:15:40 -0400

    Update and rename meme.js to Spray.js

---
+++ commands/Spray.js
@@ -0,0 +1,12 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+message.channel.send("https://i.imgur.com/LTAe2j3.png")
+
+}
+
+module.exports.help = {
+  name: "Hspray"
+}
