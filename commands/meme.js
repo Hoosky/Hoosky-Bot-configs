22f4482cc01da862106e49c1153dea60a402b0e9
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-06-17 12:27:28 -0400

    Add files via upload

---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+   let {body} = await superagent
+   .get('https://api.giphy.com/v1/gifs/random')
+
+   let dogembed = new Discord.RichEmbed()
+   .setColor("#5000ff")
+   .setTitle(":dog:")
+   .setImage(body.url);
+
+   message.channel.send(dogembed)
+
+}
+
+module.exports.help = {
+  name: "+randomgif"
+}
