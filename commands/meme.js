1b4ffdf1d19decac969052c274f876d701990dbb
Hoosky <38099698+Hoosky@users.noreply.github.com>
2018-09-11 13:56:36 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -4,14 +4,14 @@ const superagent = require("superagent");
 module.exports.run = async (bot, message, args) => {
 
    let {body} = await superagent
-   .get('https://api.imgur.com/3/gallery/r/memes')
+   .get('https://api.imgur.com/3/g/memes/viral')
 
-   let dogembed = new Discord.RichEmbed()
+   let memembed = new Discord.RichEmbed()
    .setColor("#5000ff")
-   .setTitle(":dog:")
+   .setTitle("RANDOM MEME:")
    .setImage(body.url);
 
-   message.channel.send(dogembed)
+   message.channel.send(memembed)
 
 }
 
